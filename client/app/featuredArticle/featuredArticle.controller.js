171ebb9cac45275b9ac849832c99015c9498ecc2
Brian Iversen <biversen21@gmail.com>
2014-10-30 13:13:48 -0700

    fixes sorting problem at time of vote

--- client/app/featuredArticle/featuredArticle.controller.js
+++ client/app/featuredArticle/featuredArticle.controller.js
@@ -13,11 +13,13 @@
     $scope.upvote = function(){
       $http.put('/api/news/upvote/' + $scope.news[$scope.index]._id).success(function(data){
         $scope.news[$scope.index].votes = data.votes;
+        $scope.news.sort(function(a, b) { return b.votes - a.votes});
       });
     }
     $scope.downvote = function(){
       $http.put('/api/news/downvote/' + $scope.news[$scope.index]._id).success(function(data){
         $scope.news[$scope.index].votes = data.votes;
+        $scope.news.sort(function(a, b) { return b.votes - a.votes});
       });      
     }
   }
