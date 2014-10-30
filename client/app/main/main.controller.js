f099d2cde2c6754fb9be8b8875bf6f1b92f4e6cd
Brian Iversen <biversen21@gmail.com>
2014-10-30 09:52:51 -0700

    smoother refresh of mainfactory get function

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -12,14 +12,22 @@
     $scope.index = $stateParams.index || 0;
     $state.go('main.article');
     $scope.expandedSwitch = false;
-    MainFactory.get().
-      success(function(data, status, headers, config) {
-        $scope.news = data;
-      }).
-      error(function(data, status, headers, config) {
-        // console.log(data);
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
+    $scope.news = $scope.news || [];
+    setInterval(function(){
+      MainFactory.get().
+        success(function(data, status, headers, config) {
+          for (var i = 0; i < data.length; i ++) {
+            if (!$scope.news[i]) {
+              $scope.news.push(data[i]);
+            }
+          }
+        }).
+        error(function(data, status, headers, config) {
+          // console.log(data);
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });      
+    }, 10000);
   }
+  
 }).call(this);
